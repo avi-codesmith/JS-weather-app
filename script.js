59c231195b91bfcbb8f4a2208492b2a86de8f5c8
avi-codesmith <avi.codesmith@gmail.com>
2025-03-20 18:25:02 +0530

    Start Js

--- script.js
+++ script.js
@@ -3,3 +3,9 @@
 const apiKey = "5af10595b772849237523885a0dcaf6c";
 const apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?units=metric&q=banglore";
+
+const checkWeather = async () => {
+  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+  let data = await response.json();
+  console.log(data);
+};
